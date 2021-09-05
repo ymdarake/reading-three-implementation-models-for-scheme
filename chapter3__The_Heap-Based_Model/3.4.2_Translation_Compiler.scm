12ab0eb6f3ff610ff255efe9e9f0c24930b3e9dc
ym-darake <sakamoto.takumi.dev@gmail.com>
2021-09-06 01:04:09 +0900

    [add]chapter3.4.2: Adding comment on 'conti instruction translation

--- chapter3__The_Heap-Based_Model/3.4.2_Translation_Compiler.scm
+++ chapter3__The_Heap-Based_Model/3.4.2_Translation_Compiler.scm
@@ -15,8 +15,8 @@
                           (compile test (list 'test thenc elsec)))]
                     [set! (var x)
                           (compile x (list 'assign var next))]
-                    [call/cc (x); x => lambda
-                             (let ([c (list 'conti; creates a continuation from the current stack, places this continuation in the accumulator
+                    [call/cc (x); x => lambda => 'close instruction
+                             (let ([c (list 'conti; creates a continuation from the current stack, places this continuation in the accumulator. therefore the argument of x above will be this continuation and then applied in the step below
                                             (list 'argument
                                                   (compile x '(apply))))]); sets the next expression to x.
                                (if (tail? next)
