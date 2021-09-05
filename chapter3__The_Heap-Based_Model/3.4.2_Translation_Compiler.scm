4a50d4ff6ca8fa228694fa53929561fc97f2200d
ym-darake <sakamoto.takumi.dev@gmail.com>
2021-09-06 00:35:38 +0900

    [update]chapter3.4.2: normalize single-quote

--- chapter3__The_Heap-Based_Model/3.4.2_Translation_Compiler.scm
+++ chapter3__The_Heap-Based_Model/3.4.2_Translation_Compiler.scm
@@ -2,39 +2,39 @@
   (lambda (x next)
     (cond
       [(symbol? x)
-       (list ’refer x next)]
+       (list 'refer x next)]
       [(pair? x)
        (record-case x
                     [quote (obj)
-                           (list ’constant obj next)]
+                           (list 'constant obj next)]
                     [lambda (vars body)
-                      (list ’close vars (compile body ’(return)) next)]
+                      (list 'close vars (compile body '(return)) next)]
                     [if (test then else)
                         (let ([thenc (compile then next)]
                               [elsec (compile else next)])
-                          (compile test (list ’test thenc elsec)))]
+                          (compile test (list 'test thenc elsec)))]
                     [set! (var x)
-                          (compile x (list ’assign var next))]
+                          (compile x (list 'assign var next))]
                     [call/cc (x)
-                             (let ([c (list ’conti
-                                            (list ’argument
-                                                  (compile x ’(apply))))])
+                             (let ([c (list 'conti
+                                            (list 'argument
+                                                  (compile x '(apply))))])
                                (if (tail? next)
                                    c
-                                   (list ’frame next c)))]
+                                   (list 'frame next c)))]
                     [else
                      (recur loop ([args (cdr x)]
-                                  [c (compile (car x) ’(apply))]); this line is for the Final step
+                                  [c (compile (car x) '(apply))]); this line is for the Final step
                        (if (null? args)
                            (if (tail? next)
                                c ; tail call optimization 
-                               (list ’frame next c)); Final step of the apply instruction
+                               (list 'frame next c)); Final step of the apply instruction
                            (loop (cdr args)
                                  (compile (car args)
-                                          (list ’argument c)))))])]
+                                          (list 'argument c)))))])]
       [else
-       (list ’constant x next)])))
+       (list 'constant x next)])))
 
 (define tail?
   (lambda (next)
-    (eq? (car next) ’return)))
+    (eq? (car next) 'return)))
